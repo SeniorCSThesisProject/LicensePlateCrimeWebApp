b84888582c33382580306a8b73fb6a758903bdff
Veysel Artuc <mva.artuc@gmail.com>
2023-05-13 23:43:57 +0300

    added a vehicle model

---
+++ LicensePlateCrimeWebApp/Models/Vehicle.cs
@@ -0,0 +1,42 @@
+﻿using Firebase.Auth;
+using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+using LicensePlateCrimeWebApp.Interfaces;
+using System.Buffers;
+
+namespace LicensePlateCrimeWebApp.Models
+{
+    [FirestoreData]
+    public class Vehicle : IFirebaseEntity
+    {
+        [FirestoreProperty]
+        public string Id { get; set; }
+
+        [FirestoreProperty]
+        public string OwnerId { get; set; }
+
+        public Firebase.Auth.User? Owner { get; set; }
+
+        [FirestoreProperty]
+        public string Model { get; set; }
+
+        [FirestoreProperty]
+        public string LicensePlate { get; set; }
+
+        [FirestoreProperty]
+        public string ImageUrl { get; set; }
+
+        public Vehicle(User owner, string ownerId, string model, string licensePlate, string imageUrl)
+        {
+            Owner = owner;
+            OwnerId = ownerId;
+            Model = model;
+            LicensePlate = licensePlate;
+            ImageUrl = imageUrl;
+        }
+        public Vehicle()
+        {
+
+        }
+    }
+}
