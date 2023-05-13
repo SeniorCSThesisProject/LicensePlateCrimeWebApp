5037a435e5cec77f296165442625103bd2fd6db9
Veysel Artuc <mva.artuc@gmail.com>
2023-05-13 23:41:16 +0300

    added GetCurrentUser method for a session

--- LicensePlateCrimeWebApp/GlobalHelpers/GlobalHelpers.cs
+++ LicensePlateCrimeWebApp/GlobalHelpers/GlobalHelpers.cs
@@ -1,4 +1,7 @@
-﻿namespace LicensePlateCrimeWebApp.Helpers
+﻿using Firebase.Auth;
+using Newtonsoft.Json;
+
+namespace LicensePlateCrimeWebApp.Helpers
 {
 	public static class GlobalHelpers
 	{
@@ -6,5 +9,30 @@
 		{
 			return httpContext.Session.GetString("_UserToken") != null;
 		}
+		public static User GetCurrentUser(HttpContext httpContext)
+		{
+			try
+			{
+                var json = httpContext.Session.GetString("FbUser");
+
+				if (json == null)
+				{
+					throw new NotImplementedException();
+				}
+
+                User? user = JsonConvert.DeserializeObject<User>(json);
+
+				if (user == null)
+				{
+                    throw new NotImplementedException();
+                }
+
+				return user;
+            }
+            catch (Exception e)
+			{
+				throw;
+			}
+        }
 	}
 }
