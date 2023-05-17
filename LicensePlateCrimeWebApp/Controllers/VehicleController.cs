611ef25b537d55a59bd5abc2c2651c77007bdd17
Veysel Artuc <mva.artuc@gmail.com>
2023-05-17 22:21:47 +0300

    implemented vehicle selection based on user ownership

--- LicensePlateCrimeWebApp/Controllers/VehicleController.cs
+++ LicensePlateCrimeWebApp/Controllers/VehicleController.cs
@@ -1,4 +1,4 @@
-﻿using LicensePlateCrimeWebApp.Helpers;
+﻿using LicensePlateCrimeWebApp.Data;
 using LicensePlateCrimeWebApp.Interfaces;
 using LicensePlateCrimeWebApp.Models;
 using LicensePlateCrimeWebApp.ViewModels;
@@ -8,101 +8,102 @@ using Microsoft.AspNetCore.Mvc;
 namespace LicensePlateCrimeWebApp.Controllers
 {
 
-  [Authorize]
-  public class VehicleController : Controller
-  {
-    private readonly IVehicleRepository _vehicleRepository;
-    private readonly IHttpContextAccessor _httpContextAccessor;
+	[Authorize]
+	public class VehicleController : Controller
+	{
+		private readonly FirebaseAppProvider _firebaseAppProvider;
+		private readonly IVehicleRepository _vehicleRepository;
 
-    public VehicleController(IVehicleRepository vehicleRepository, IHttpContextAccessor httpContextAccessor)
-    {
-      _vehicleRepository = vehicleRepository;
-      _httpContextAccessor = httpContextAccessor;
-    }
-    // GET: Vehicles
-    public async Task<ActionResult> Index()
-    {
-      var vehicles = await _vehicleRepository.GetAllAsync();
-      return View(vehicles);
-    }
+		public VehicleController(IVehicleRepository vehicleRepository, FirebaseAppProvider firebaseAppProvider)
+		{
+			_vehicleRepository = vehicleRepository;
+			_firebaseAppProvider = firebaseAppProvider;
+		}
+		// GET: Vehicles
+		public async Task<ActionResult> Index()
+		{
+			//var vehicles = await _vehicleRepository.GetAllAsync();
+			var vehicles = await _vehicleRepository.GetAllFromUserIdAsync(_firebaseAppProvider.FirebaseAuthClient.User.Uid);
+			return View(vehicles);
+		}
 
-    // GET: Vehicles/Details/5
-    public ActionResult Details(int id)
-    {
-      return View();
-    }
+		// GET: Vehicles/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
 
-    // GET: Vehicles/Create
-    public ActionResult Create()
-    {
-      var currentUser = GlobalHelpers.GetCurrentUserInfo(_httpContextAccessor.HttpContext);
-      var currentUserId = currentUser.Uid;
-      var createVehicleViewModel = new CreateVehicleViewModel
-      {
-        OwnerId = currentUserId
-      };
-      return View(createVehicleViewModel);
-    }
+		// GET: Vehicles/Create
+		public ActionResult Create()
+		{
+			var currentUser = _firebaseAppProvider.FirebaseAuthClient.User;
+			var currentUserId = currentUser.Uid;
+			var createVehicleViewModel = new CreateVehicleViewModel
+			{
+				OwnerId = currentUserId
+			};
+			return View(createVehicleViewModel);
+		}
 
-    // POST: Vehicles/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<ActionResult> Create(CreateVehicleViewModel createVehicleViewModel)
-    {
-      if (ModelState.IsValid)
-      {
-        var uploadedImgUrl = await _vehicleRepository.UploadVehicleImgAsync(createVehicleViewModel.Image);
-        var vehicle = new Vehicle(null, createVehicleViewModel.OwnerId, createVehicleViewModel.Model, createVehicleViewModel.LicensePlate, uploadedImgUrl);
-        await _vehicleRepository.AddAsync(vehicle);
-        return RedirectToAction("Index");
-      }
-      else
-      {
-        ModelState.AddModelError("", "Photo Upload Failed");
-        return View(createVehicleViewModel);
-      }
-    }
+		// POST: Vehicles/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create(CreateVehicleViewModel createVehicleViewModel)
+		{
+			if (ModelState.IsValid)
+			{
+				var uploadedImgUrl = await _vehicleRepository.UploadVehicleImgAsync(createVehicleViewModel.Image);
+				var vehicle = new Vehicle(null, createVehicleViewModel.OwnerId, createVehicleViewModel.Model, createVehicleViewModel.LicensePlate, uploadedImgUrl);
+				await _vehicleRepository.AddAsync(vehicle);
+				return RedirectToAction("Index");
+			}
+			else
+			{
+				ModelState.AddModelError("", "Photo Upload Failed");
+				return View(createVehicleViewModel);
+			}
+		}
 
-    // GET: Vehicles/Edit/5
-    public ActionResult Edit(int id)
-    {
-      return View();
-    }
+		// GET: Vehicles/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-    // POST: Vehicles/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
+		// POST: Vehicles/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-    // GET: Vehicles/Delete/5
-    public ActionResult Delete(int id)
-    {
-      return View();
-    }
+		// GET: Vehicles/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
 
-    // POST: Vehicles/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
-  }
+		// POST: Vehicles/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
--- LicensePlateCrimeWebApp/Interfaces/IVehicleRepository.cs
+++ LicensePlateCrimeWebApp/Interfaces/IVehicleRepository.cs
@@ -2,17 +2,18 @@
 
 namespace LicensePlateCrimeWebApp.Interfaces
 {
-  public interface IVehicleRepository
-  {
-    Task<IEnumerable<Vehicle>> GetAllAsync();
-    Task<Vehicle> GetByIdAsync(string id);
-    Task<Vehicle> GetByIdNoTrackingAsync(string id);
-    Task<Vehicle> GetByLicensePlateAsync(string id);
+	public interface IVehicleRepository
+	{
+		Task<IEnumerable<Vehicle>> GetAllAsync();
+		Task<IEnumerable<Vehicle>> GetAllFromUserIdAsync(string id);
+		Task<Vehicle> GetByIdAsync(string id);
+		Task<Vehicle> GetByIdNoTrackingAsync(string id);
+		Task<Vehicle> GetByLicensePlateAsync(string id);
 
-    Task<string> UploadVehicleImgAsync(IFormFile imgFile);
-    Task<bool> AddAsync(Vehicle vehicle);
-    Task<bool> UpdateAsync(Vehicle vehicle);
-    Task<bool> DeleteAsync(string id);
-    Task<bool> SaveAsync();
-  }
+		Task<string> UploadVehicleImgAsync(IFormFile imgFile);
+		Task<bool> AddAsync(Vehicle vehicle);
+		Task<bool> UpdateAsync(Vehicle vehicle);
+		Task<bool> DeleteAsync(string id);
+		Task<bool> SaveAsync();
+	}
 }
