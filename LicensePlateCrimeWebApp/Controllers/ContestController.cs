d1c8b57109ab026802a54644f99c197f511059d3
Veysel Artuc <mva.artuc@gmail.com>
2023-05-17 21:56:14 +0300

    added authorization requirements to contest and violation controllers

--- LicensePlateCrimeWebApp/Controllers/ContestController.cs
+++ LicensePlateCrimeWebApp/Controllers/ContestController.cs
@@ -1,82 +1,84 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LicensePlateCrimeWebApp.Controllers
 {
-  public class ContestController : Controller
-  {
-    // GET: Contests
-    public ActionResult Index()
-    {
-      return View();
-    }
+	[Authorize]
+	public class ContestController : Controller
+	{
+		// GET: Contests
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-    // GET: Contests/Details/5
-    public ActionResult Details(int id)
-    {
-      return View();
-    }
+		// GET: Contests/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
 
-    // GET: Contests/Create
-    public ActionResult Create()
-    {
-      return View();
-    }
+		// GET: Contests/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-    // POST: Contests/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create(IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
+		// POST: Contests/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-    // GET: Contests/Edit/5
-    public ActionResult Edit(int id)
-    {
-      return View();
-    }
+		// GET: Contests/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-    // POST: Contests/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
+		// POST: Contests/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-    // GET: Contests/Delete/5
-    public ActionResult Delete(int id)
-    {
-      return View();
-    }
+		// GET: Contests/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
 
-    // POST: Contests/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
-  }
+		// POST: Contests/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
--- LicensePlateCrimeWebApp/Controllers/ViolationController.cs
+++ LicensePlateCrimeWebApp/Controllers/ViolationController.cs
@@ -1,82 +1,84 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LicensePlateCrimeWebApp.Controllers
 {
-  public class ViolationController : Controller
-  {
-    // GET: Violations
-    public ActionResult Index()
-    {
-      return View();
-    }
+	[Authorize]
+	public class ViolationController : Controller
+	{
+		// GET: Violations
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-    // GET: Violations/Details/5
-    public ActionResult Details(int id)
-    {
-      return View();
-    }
+		// GET: Violations/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
 
-    // GET: Violations/Create
-    public ActionResult Create()
-    {
-      return View();
-    }
+		// GET: Violations/Create
+		public ActionResult Create()
+		{
+			return View();
+		}
 
-    // POST: Violations/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create(IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
+		// POST: Violations/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-    // GET: Violations/Edit/5
-    public ActionResult Edit(int id)
-    {
-      return View();
-    }
+		// GET: Violations/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
 
-    // POST: Violations/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
+		// POST: Violations/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
 
-    // GET: Violations/Delete/5
-    public ActionResult Delete(int id)
-    {
-      return View();
-    }
+		// GET: Violations/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
 
-    // POST: Violations/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
-  }
+		// POST: Violations/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
 }
