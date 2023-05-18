a0700421ba5daf13af73f0bf640201652d792f64
Veysel Artuc <mva.artuc@gmail.com>
2023-05-18 21:26:55 +0300

    implemented detail view on vehicles, styled the view

--- LicensePlateCrimeWebApp/Controllers/VehicleController.cs
+++ LicensePlateCrimeWebApp/Controllers/VehicleController.cs
@@ -8,103 +8,104 @@ using Microsoft.AspNetCore.Mvc;
 namespace LicensePlateCrimeWebApp.Controllers
 {
 
-	[Authorize]
-	public class VehicleController : Controller
-	{
-		private readonly FirebaseAppProvider _firebaseAppProvider;
-		private readonly IVehicleRepository _vehicleRepository;
+  [Authorize]
+  public class VehicleController : Controller
+  {
+    private readonly FirebaseAppProvider _firebaseAppProvider;
+    private readonly IVehicleRepository _vehicleRepository;
 
-		public VehicleController(IVehicleRepository vehicleRepository, FirebaseAppProvider firebaseAppProvider)
-		{
-			_vehicleRepository = vehicleRepository;
-			_firebaseAppProvider = firebaseAppProvider;
-		}
-		// GET: Vehicles
-		public async Task<ActionResult> Index()
-		{
-			//var vehicles = await _vehicleRepository.GetAllAsync();
-			var vehicles = await _vehicleRepository.GetAllFromUserIdAsync(_firebaseAppProvider.FirebaseAuthClient.User.Uid);
-			return View(vehicles);
-		}
+    public VehicleController(IVehicleRepository vehicleRepository, FirebaseAppProvider firebaseAppProvider)
+    {
+      _vehicleRepository = vehicleRepository;
+      _firebaseAppProvider = firebaseAppProvider;
+    }
+    // GET: Vehicles
+    public async Task<ActionResult> Index()
+    {
+      //var vehicles = await _vehicleRepository.GetAllAsync();
+      var vehicles = await _vehicleRepository.GetAllFromUserIdAsync(_firebaseAppProvider.FirebaseAuthClient.User.Uid);
+      return View(vehicles);
+    }
 
-		// GET: Vehicles/Details/5
-		public ActionResult Details(int id)
-		{
-			return View();
-		}
+    // GET: Vehicles/Details/5
+    public async Task<ActionResult> Detail(string id)
+    {
+      var vehicle = await _vehicleRepository.GetByIdAsync(id);
+      return View(vehicle);
+    }
 
-		// GET: Vehicles/Create
-		public ActionResult Create()
-		{
-			var currentUser = _firebaseAppProvider.FirebaseAuthClient.User;
-			var currentUserId = currentUser.Uid;
-			var createVehicleViewModel = new CreateVehicleViewModel
-			{
-				OwnerId = currentUserId
-			};
-			return View(createVehicleViewModel);
-		}
+    // GET: Vehicles/Create
+    public ActionResult Create()
+    {
+      var currentUser = _firebaseAppProvider.FirebaseAuthClient.User;
+      var currentUserId = currentUser.Uid;
+      var createVehicleViewModel = new CreateVehicleViewModel
+      {
+        OwnerId = currentUserId
+      };
+      return View(createVehicleViewModel);
+    }
 
-		// POST: Vehicles/Create
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> Create(CreateVehicleViewModel createVehicleViewModel)
-		{
-			if (ModelState.IsValid)
-			{
-				var uploadedImgUrl = await _vehicleRepository.UploadVehicleImgAsync(createVehicleViewModel.Image);
+    // POST: Vehicles/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Create(CreateVehicleViewModel createVehicleViewModel)
+    {
+      if (ModelState.IsValid)
+      {
+        var uploadedImgUrl = await _vehicleRepository.UploadVehicleImgAsync(createVehicleViewModel.Image);
         var vehicle = new Vehicle(createVehicleViewModel.OwnerId, createVehicleViewModel.Model, createVehicleViewModel.LicensePlate, uploadedImgUrl);
         var id = await _vehicleRepository.AddAsync(vehicle);
         vehicle.Id = id;
-				return RedirectToAction("Index");
-			}
-			else
-			{
-				ModelState.AddModelError("", "Photo Upload Failed");
-				return View(createVehicleViewModel);
-			}
-		}
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        ModelState.AddModelError("", "Photo Upload Failed");
+        return View(createVehicleViewModel);
+      }
+    }
 
-		// GET: Vehicles/Edit/5
-		public ActionResult Edit(int id)
-		{
-			return View();
-		}
+    // GET: Vehicles/Edit/5
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
 
-		// POST: Vehicles/Edit/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit(int id, IFormCollection collection)
-		{
-			try
-			{
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
+    // POST: Vehicles/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
 
-		// GET: Vehicles/Delete/5
-		public ActionResult Delete(int id)
-		{
-			return View();
-		}
+    // GET: Vehicles/Delete/5
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
 
-		// POST: Vehicles/Delete/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Delete(int id, IFormCollection collection)
-		{
-			try
-			{
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
-	}
+    // POST: Vehicles/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
+  }
 }
