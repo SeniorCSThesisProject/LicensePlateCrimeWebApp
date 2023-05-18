5f5ed9000aa9fd1e87c6eacea86d82bc4509c4c0
Veysel Artuc <mva.artuc@gmail.com>
2023-05-18 21:25:38 +0300

    removed unnecessary namespaces

--- LicensePlateCrimeWebApp/Controllers/ContactController.cs
+++ LicensePlateCrimeWebApp/Controllers/ContactController.cs
@@ -1,104 +1,99 @@
-﻿using LicensePlateCrimeWebApp.Helpers;
-using LicensePlateCrimeWebApp.Interfaces;
+﻿using LicensePlateCrimeWebApp.Interfaces;
 using LicensePlateCrimeWebApp.Models;
-using LicensePlateCrimeWebApp.Repository;
-using LicensePlateCrimeWebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using System.Xml.Linq;
 
 namespace LicensePlateCrimeWebApp.Controllers
 {
-    public class ContactController : Controller
+  public class ContactController : Controller
+  {
+    private readonly IContactRepository _contactRepository;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public ContactController(IContactRepository contactRepository, IHttpContextAccessor httpContextAccessor)
     {
-		private readonly IContactRepository _contactRepository;
-		private readonly IHttpContextAccessor _httpContextAccessor;
+      _contactRepository = contactRepository;
+      _httpContextAccessor = httpContextAccessor;
+    }
+    // GET: Contact
+    public async Task<ActionResult> Index()
+    {
+      return View();
+    }
 
-		public ContactController(IContactRepository contactRepository, IHttpContextAccessor httpContextAccessor)
-		{
-			_contactRepository = contactRepository;
-			_httpContextAccessor = httpContextAccessor;
-		}
-		// GET: Contact
-		public async Task<ActionResult> Index()
-		{
-			return View();
-		}
+    // GET: Contact/Details/5
+    public ActionResult Details(int id)
+    {
+      return View();
+    }
 
-		// GET: Contact/Details/5
-		public ActionResult Details(int id)
-		{
-			return View();
-		}
+    // GET: Contact/Create
+    public ActionResult Create()
+    {
+      var createcontactModel = new Contact();
+      return View(createcontactModel);
+    }
 
-		// GET: Contact/Create
-		public ActionResult Create()
-		{
-			var createcontactModel = new Contact();
-			return View(createcontactModel);
-		}
+    // POST: Contact/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Create(Contact createcontactModel)
+    {
+      if (ModelState.IsValid)
+      {
 
-		// POST: Contact/Create
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> Create(Contact createcontactModel)
-		{
-			if (ModelState.IsValid)
-			{
-			
         var contact = new Contact(createcontactModel.Name, createcontactModel.Email, createcontactModel.Telephone, createcontactModel.Subject, createcontactModel.Message);
         var id = await _contactRepository.AddAsync(contact);
         contact.Id = id;
-				return RedirectToAction("Index");
-			}
-			else
-			{
-				ModelState.AddModelError("", "Mesaj gönderme sırasında hata!!! Tekrar deneyiniz.");
-				return View("Index", createcontactModel);
-			}
-		}
+        return RedirectToAction("Index");
+      }
+      else
+      {
+        ModelState.AddModelError("", "Mesaj gönderme sırasında hata!!! Tekrar deneyiniz.");
+        return View("Index", createcontactModel);
+      }
+    }
 
-		// GET: Contact/Edit/5
-		public ActionResult Edit(int id)
-		{
-			return View();
-		}
+    // GET: Contact/Edit/5
+    public ActionResult Edit(int id)
+    {
+      return View();
+    }
 
-		// POST: Contact/Edit/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Edit(int id, IFormCollection collection)
-		{
-			try
-			{
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
+    // POST: Contact/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
 
-		// GET: Contact/Delete/5
-		public ActionResult Delete(int id)
-		{
-			return View();
-		}
+    // GET: Contact/Delete/5
+    public ActionResult Delete(int id)
+    {
+      return View();
+    }
 
-		// POST: Contact/Delete/5
-		[HttpPost]
-		[ValidateAntiForgeryToken]
-		public ActionResult Delete(int id, IFormCollection collection)
-		{
-			try
-			{
-				return RedirectToAction(nameof(Index));
-			}
-			catch
-			{
-				return View();
-			}
-		}
+    // POST: Contact/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+      try
+      {
+        return RedirectToAction(nameof(Index));
+      }
+      catch
+      {
+        return View();
+      }
+    }
 
-	}
+  }
 }
