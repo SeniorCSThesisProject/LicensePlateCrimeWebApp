9eb9c060ab43ee5a2c6a75156c6b885db11ae39f
Veysel Artuc <mva.artuc@gmail.com>
2023-05-18 21:22:25 +0300

    brought back IFirestore Entity and Id fields on FirestoreProvider

---
+++ LicensePlateCrimeWebApp/Interfaces/IFirestoreEntity.cs
@@ -0,0 +1,15 @@
+﻿using Google.Cloud.Firestore;
+
+namespace LicensePlateCrimeWebApp.Interfaces
+{
+  public interface IFirestoreEntity
+  {
+    public string Id { get; set; }
+
+    [FirestoreProperty]
+    public DateTime? CreationDate { get; set; }
+
+    [FirestoreProperty]
+    public DateTime? LastUpdateDate { get; set; }
+  }
+}
