f9e62247577e9de736dbd174993cae6ea5fbfa42
raifakyol <raifakyol61@hotmail.com>
2023-05-27 11:09:15 +0300

    added error controller

---
+++ LicensePlateCrimeWebApp/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LicensePlateCrimeWebApp.Controllers
+{
+  public class ErrorController : Controller
+  {
+    public IActionResult Error(int code)
+    {
+      return View();
+    }
+  }
+}
