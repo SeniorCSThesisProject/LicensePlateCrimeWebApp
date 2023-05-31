7de0c96a22573ec2bd37133f562031ffb6d71ce1
Veysel Artuc <mva.artuc@gmail.com>
2023-05-31 17:40:55 +0300

    removed unused code on ViolationController

--- LicensePlateCrimeWebApp/Controllers/ViolationController.cs
+++ LicensePlateCrimeWebApp/Controllers/ViolationController.cs
@@ -37,74 +37,11 @@ namespace LicensePlateCrimeWebApp.Controllers
       return View(violations);
     }
 
-    // GET: Violations/Details/5
+    // GET: Violations/Detail/5
     public async Task<ActionResult> Detail(string id)
     {
       var violation = await _violationRepository.GetByIdAsync(id);
       return View(violation);
-    }
-
-    // GET: Violations/Create
-    public ActionResult Create()
-    {
-      return View();
-    }
-
-    // POST: Violations/Create
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create(IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
-
-    // GET: Violations/Edit/5
-    public ActionResult Edit(int id)
-    {
-      return View();
-    }
-
-    // POST: Violations/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
-    }
-
-    // GET: Violations/Delete/5
-    public ActionResult Delete(int id)
-    {
-      return View();
-    }
-
-    // POST: Violations/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-      try
-      {
-        return RedirectToAction(nameof(Index));
-      }
-      catch
-      {
-        return View();
-      }
     }
   }
 }
